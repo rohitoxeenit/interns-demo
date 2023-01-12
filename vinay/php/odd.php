3de8f9371b9f5cdea756b26786b8169e4e4f6993
Rohit Dhiman <rohit@oxeenit.com>
2023-01-12 18:39:49 +0530

    Create odd.php

---
+++ vinay/php/odd.php
@@ -0,0 +1,17 @@
+<!doctype>
+<html>
+<head>
+      <h1>odd number</h1>
+       <body>
+<?php
+     
+	  $x = 1;
+	  while($x<20){
+	 echo "<br>";
+	   echo " The number is:$x <br>";
+	  $x+=2;	    
+}
+?>
+</body>
+</html>  	  
+
