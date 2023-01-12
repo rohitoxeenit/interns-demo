f597e20ba0a0ebf2d10c0440b12ab73f5adc5093
Rohit Dhiman <rohit@oxeenit.com>
2023-01-12 18:39:31 +0530

    Create try4.php

---
+++ himanshu/php/try4.php
@@ -0,0 +1,26 @@
+<!doctype>
+<html>
+<body>
+<?php  
+$count = 0;  
+$num = 2;  
+while ($count < 25 )  
+{  
+$div_count=0;  
+for ( $i=1; $i<=$num; $i++)  
+{  
+if (($num%$i)==0)  
+{  
+$div_count++;  
+}  
+}  
+if ($div_count<3)  
+{  
+echo $num." , ";  
+$count=$count+1;  
+}  
+$num=$num+1;  
+}  
+?>  
+</body>
+</html>
