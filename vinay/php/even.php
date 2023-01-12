5eba77e03ea31cb227f07c4662a63000b1805244
Rohit Dhiman <rohit@oxeenit.com>
2023-01-12 18:39:37 +0530

    Create even.php

---
+++ vinay/php/even.php
@@ -0,0 +1,20 @@
+<!doctype>
+<html>
+<head>
+      <h1>even number</h1>
+       <body>
+<?php
+     
+	  $x = 0;
+	  while($x<20){
+	 echo "<br>";
+	   echo "The number is:$x <br>";
+	  $x+=2;	             
+	}
+?>
+</body>
+</html>  	  
+	  
+  	  
+
+
