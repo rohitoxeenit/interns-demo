a5effc1b06670f3534af3379e263b6ccc59398f3
Rohit Dhiman <rohit@oxeenit.com>
2023-01-16 18:32:13 +0530

    Create php-demo4.php

---
+++ demolectures/phptutorial/php-demo4.php
@@ -0,0 +1,23 @@
+ <?php
+$x = 1;
+do {
+	echo $x=$x+1;
+} while ($x <= 5);
+
+
+$y = 1;
+do {
+	echo "<br/>";
+	echo $y=$y+1;
+} while ($y >= 5);
+
+echo  "<br/>";echo  "<br/>";
+
+$yz = 1;
+while($yz <= 5){
+	
+	echo $yz;
+	echo  "<br/>";
+	$yz=$yz+1;
+}
+?> 
