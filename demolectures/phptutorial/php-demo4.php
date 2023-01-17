64e9191cd37f28c89248b6b471236a23d824291c
Rohit Dhiman <rohit@oxeenit.com>
2023-01-17 15:20:47 +0530

    Update php-demo4.php

--- demolectures/phptutorial/php-demo4.php
+++ demolectures/phptutorial/php-demo4.php
@@ -1,23 +1,28 @@
- <?php
-$x = 1;
-do {
-	echo $x=$x+1;
-} while ($x <= 5);
+<!DOCTYPE html>
+<html>
+<body>
 
-
-$y = 1;
-do {
-	echo "<br/>";
-	echo $y=$y+1;
-} while ($y >= 5);
-
-echo  "<br/>";echo  "<br/>";
-
-$yz = 1;
-while($yz <= 5){
-	
-	echo $yz;
-	echo  "<br/>";
-	$yz=$yz+1;
+<?php
+$cars = array (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+);
+$i=0;    
+foreach( $cars as $k){
+	echo 'Array Loop Count: '.$i;
+	 echo "<br/>"; echo "<br/>";
+ foreach ($k as $car_array_keys => $car_value){
+	 //echo "<pre>";
+	 //print_r($car_value);
+	 echo 'Array Key: '.$car_array_keys.' == '.$car_value;
+	 echo "<br/>";
+ }
+  echo "<br/>";
+$i++;
 }
-?> 
+?>
+
+</body>
+</html>
